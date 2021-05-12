b90f6b564f5bdd7fd8a553103dd984dd67042d09
Aleksandar Ristic <aristic665@gmail.com>
2021-05-13 01:20:49 +0200

    Doing posts, postitems

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -9,6 +9,7 @@ import AddExperience from '../profile-forms/AddExperience';
 import AddEducation from '../profile-forms/AddEducation';
 import Profiles from '../profiles/Profiles';
 import Profile from '../profile/Profile';
+import Posts from '../posts/Posts';
 import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
 
@@ -21,11 +22,13 @@ const Routes = props => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/profiles" component={Profiles} />
         <Route exact path="/profile/:id" component={Profile} />
+
         <PrivateRoute exact path="/dashboard" component={Dashboard} />    
         <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
         <PrivateRoute exact path="/add-experience" component={AddExperience} />
         <PrivateRoute exact path="/add-education" component={AddEducation} />
+        <PrivateRoute exact path="/posts" component={Posts} />
         <Route component={NotFound} />
       </Switch>
     </section>
