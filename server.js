06249b6182a9f73f49d137d716325dcfccadc26c
Aleksandar <aristic665@gmail.com>
2021-05-14 04:16:39 +0200

    Prepared for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 // Connect Database
@@ -17,7 +18,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(epress.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
